a560e44f84c70186f205f6664e130b9e0dff93b2
0x00-ketsu <ketsu.chin.121@gmail.com>
2023-09-26 15:35:57 +0800

    feat: add autocmd `QuitPre`, restore when only one window in tabpage

--- lua/maximizer/init.lua
+++ lua/maximizer/init.lua
@@ -1,10 +1,29 @@
+local api = vim.api
+
 local config = require('maximizer.config')
 local window = require('maximizer.window')
+
+local function load_autocmd()
+  api.nvim_create_autocmd(
+      {'QuitPre'}, {
+        pattern = {'*'},
+        callback = function()
+          -- Restore when only one window in tabpage
+          local curwins = api.nvim_tabpage_list_wins(0)
+          if vim.t.is_maximized and #curwins == 2 then
+            window.restore()
+          end
+        end
+      }
+  )
+end
 
 local M = {}
 
 M.setup = function(opts)
   config.setup(opts)
+
+  load_autocmd()
 end
 
 ---Toggle (maximize/restore) for current window
