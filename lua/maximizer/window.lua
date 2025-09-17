a2bc01d1331673cdfea2f52eb22fd36e95a17bb7
ketsu <ketsu.chin.121@gmail.com>
2025-09-17 15:32:43 +0800

    refactor: enhance window maximizer functionality

--- lua/maximizer/window.lua
+++ lua/maximizer/window.lua
@@ -5,9 +5,36 @@ end
 
 ---@param win integer
 local function disable_sidebar_features(win)
-  vim.api.nvim_win_set_option(win, 'number', false)
-  vim.api.nvim_win_set_option(win, 'relativenumber', false)
-  vim.api.nvim_win_set_option(win, 'signcolumn', 'no')
+  vim.api.nvim_set_option_value("number", false, { win = win })
+  vim.api.nvim_set_option_value("relativenumber", false, { win = win })
+  vim.api.nvim_set_option_value("signcolumn", "no", { win = win })
+  vim.api.nvim_set_option_value("colorcolumn", "", { win = win })
+  vim.api.nvim_set_option_value("foldcolumn", "0", { win = win })
+
+  if pcall(vim.api.nvim_get_option_value, "winbar", { win = win }) then
+    vim.api.nvim_set_option_value("winbar", "", { win = win })
+  end
+
+  if vim.fn.has("nvim-0.9") == 1 then
+    vim.api.nvim_set_option_value("statuscolumn", "", { win = win })
+  end
+end
+
+---@param win integer
+local function get_sidebar_settings(win)
+  return {
+    signcolumn = vim.api.nvim_get_option_value("signcolumn", { win = win }),
+    number = vim.api.nvim_get_option_value("number", { win = win }),
+    relativenumber = vim.api.nvim_get_option_value("relativenumber", { win = win }),
+    foldcolumn = vim.api.nvim_get_option_value("foldcolumn", { win = win }),
+    colorcolumn = vim.api.nvim_get_option_value("colorcolumn", { win = win }),
+    winbar = pcall(vim.api.nvim_get_option_value, "winbar", { win = win })
+        and vim.api.nvim_get_option_value("winbar", { win = win })
+      or nil,
+    statuscolumn = vim.fn.has("nvim-0.9") == 1
+        and vim.api.nvim_get_option_value("statuscolumn", { win = win })
+      or nil,
+  }
 end
 
 ---@return table
@@ -15,11 +42,7 @@ local function get_window_settings()
   local win_settings = {}
   local wins = vim.api.nvim_tabpage_list_wins(0)
   for _, win in ipairs(wins) do
-    win_settings[tostring(win)] = {
-      signcolumn = vim.api.nvim_win_get_option(win, 'signcolumn'),
-      number = vim.api.nvim_win_get_option(win, 'number'),
-      relativenumber = vim.api.nvim_win_get_option(win, 'relativenumber'),
-    }
+    win_settings[tostring(win)] = get_sidebar_settings(win)
   end
   return win_settings
 end
@@ -40,7 +63,7 @@ local function maximize_off()
     local winnr = tonumber(winnr, 10)
     if vim.api.nvim_win_is_valid(winnr) then
       for name, value in pairs(options) do
-        vim.api.nvim_win_set_option(winnr, name, value)
+        vim.api.nvim_set_option_value(name, value, { win = winnr })
       end
     end
   end
@@ -58,17 +81,22 @@ end
 
 M.maximize = function()
   -- Do nothing if only one window
-  if vim.fn.winnr('$') == 1 then
+  if vim.fn.winnr("$") == 1 then
     set_maximized(false)
     return
   end
 
   vim.t.mx_sizes = vim.fn.winrestcmd()
-  vim.cmd('vert resize | resize')
+
+  -- Resize current window to maximum
+  local cur_win = vim.fn.win_getid()
+  local max_height = vim.o.lines - vim.o.cmdheight - 2 -- 2 for tabline/statusline
+  local max_width = vim.o.columns
+  vim.api.nvim_win_set_height(cur_win, max_height)
+  vim.api.nvim_win_set_width(cur_win, max_width)
 
   local win_settings = get_window_settings()
   vim.t.mx_win_settings = win_settings
-
   if next(win_settings) then
     maximize_on()
     set_maximized(true)
